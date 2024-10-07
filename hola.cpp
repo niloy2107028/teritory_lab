0d66cd2729b9c5167b8c92b9d7c1b80ff23c8ad3
Sohaib Hasan Niloy <122288265+niloy2107028@users.noreply.github.com>
2024-10-07 14:06:11 +0600

    Update hola.cpp

--- hola.cpp
+++ hola.cpp
@@ -1,150 +1,97 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
-
-const double epsilon = 1e-9;
 
 using namespace std;
 
-void printing_matrix(vector<vector<double>> matrix)
+void lu_factorizing(const vector<vector<float>> &matrix, vector<vector<float>> &l_matrix, vector<vector<float>> &u_matrix)
 {
-    cout << "printing matrix :" << endl;
-    for (const auto row : matrix)
-    {
-        for (double element : row)
-        {
-            cout << element << " ";
-        }
-        cout << endl;
-    }
+    u_matrix[0][0] = matrix[0][0];
+    u_matrix[0][1] = matrix[0][1];
+    u_matrix[0][2] = matrix[0][2];
+    u_matrix[0][3] = matrix[0][3];
+
+    l_matrix[1][0] = matrix[1][0] / u_matrix[0][0];
+    u_matrix[1][1] = matrix[1][1] - l_matrix[1][0] * u_matrix[0][1];
+    u_matrix[1][2] = matrix[1][2] - l_matrix[1][0] * u_matrix[0][2];
+    u_matrix[1][3] = matrix[1][3] - l_matrix[1][0] * u_matrix[0][3];
+
+    l_matrix[2][0] = matrix[2][0] / u_matrix[0][0];
+    l_matrix[2][1] = (matrix[2][1] - l_matrix[2][0] * u_matrix[0][1]) / u_matrix[1][1];
+    u_matrix[2][2] = matrix[2][2] - l_matrix[2][0] * u_matrix[0][2] - l_matrix[2][1] * u_matrix[1][2];
+    u_matrix[2][3] = matrix[2][3] - l_matrix[2][0] * u_matrix[0][3] - l_matrix[2][1] * u_matrix[1][3];
+
+    l_matrix[3][0] = matrix[3][0] / u_matrix[0][0];
+    l_matrix[3][1] = (matrix[3][1] - l_matrix[3][0] * u_matrix[0][1]) / u_matrix[1][1];
+    l_matrix[3][2] = (matrix[3][2] - l_matrix[3][0] * u_matrix[0][2] - l_matrix[3][1] * u_matrix[1][2]) / u_matrix[2][2];
+    u_matrix[3][3] = matrix[3][3] - l_matrix[3][0] * u_matrix[0][3] - l_matrix[3][1] * u_matrix[1][3] - l_matrix[3][2] * u_matrix[2][3];
+
+    l_matrix[0][0] = 1;
+    l_matrix[1][1] = 1;
+    l_matrix[2][2] = 1;
+    l_matrix[3][3] = 1;
 }
 
-void printing_inverse_matrix(vector<vector<double>> matrix)
+vector<float> solve_ly(const vector<vector<float>> &l_matrix, const vector<float> &b)
 {
-    cout << "printing matrix :" << endl;
-    for (int i = 0; i < matrix.size(); i++)
-    {
-        for (int j = matrix.size(); j < matrix.size() * 2; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+    vector<float> y(4, 0);
+    y[0] = b[0];
+    y[1] = b[1] - l_matrix[1][0] * y[0];
+    y[2] = b[2] - l_matrix[2][0] * y[0] - l_matrix[2][1] * y[1];
+    y[3] = b[3] - l_matrix[3][0] * y[0] - l_matrix[3][1] * y[1] - l_matrix[3][2] * y[2];
+    return y;
 }
 
-void gauss_eli(vector<vector<double>> &matrix)
+vector<float> solve_ux(const vector<vector<float>> &u_matrix, const vector<float> &y)
 {
-    int size = matrix.size();
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = i + 1; j < size; j++)
-        {
-            if (abs(matrix[i][i]) < epsilon)
-            {
-                cout << "Error! Near zero pivot element at row " << i << " (element: " << matrix[i][i] << ")." << endl;
-                exit(EXIT_FAILURE);
-            }
-
-            double factor = matrix[j][i] / matrix[i][i];
-
-            for (int k = i; k < size * 2; k++)
-            {
-                matrix[j][k] -= factor * matrix[i][k];
-            }
-        }
-    }
+    vector<float> x(4, 0);
+    x[3] = y[3] / u_matrix[3][3];
+    x[2] = (y[2] - u_matrix[2][3] * x[3]) / u_matrix[2][2];
+    x[1] = (y[1] - u_matrix[1][2] * x[2] - u_matrix[1][3] * x[3]) / u_matrix[1][1];
+    x[0] = (y[0] - u_matrix[0][1] * x[1] - u_matrix[0][2] * x[2] - u_matrix[0][3] * x[3]) / u_matrix[0][0];
+    return x;
 }
 
-void jordan_eli(vector<vector<double>> &matrix)
+int main()
 {
-    int size = matrix.size();
+    int size_of_matrix = 4;
+    vector<vector<float>> matrix(size_of_matrix, vector<float>(size_of_matrix + 1));
+    vector<float> b(size_of_matrix);
 
-    for (int i = size - 1; i > -1; i--)
+    for (int i = 0; i < size_of_matrix; i++)
     {
-        for (int j = i - 1; j > -1; j--)
+        for (int j = 0; j < size_of_matrix + 1; j++)
         {
-            if (abs(matrix[i][i]) < epsilon)
+            cin >> matrix[i][j];
+            if (j == size_of_matrix)
             {
-                cout << "Error! Near zero pivot element at row " << i << " (element: " << matrix[i][i] << ")." << endl;
-                exit(EXIT_FAILURE);
-            }
-
-            double factor = matrix[j][i] / matrix[i][i];
-
-            for (int k = size * 2 - 1; k > -1; k--)
-            {
-                matrix[j][k] -= factor * matrix[i][k];
+                b[i] = matrix[i][j];
             }
         }
     }
-}
 
-void row_eche(vector<vector<double>> &matrix)
-{
-    int size = matrix.size();
-    for (int i = 0; i < size; i++)
+    vector<vector<float>> l_matrix(size_of_matrix, vector<float>(size_of_matrix, 0));
+    vector<vector<float>> u_matrix(size_of_matrix, vector<float>(size_of_matrix, 0));
+
+    lu_factorizing(matrix, l_matrix, u_matrix);
+
+    vector<float> y = solve_ly(l_matrix, b);
+    vector<float> x = solve_ux(u_matrix, y);
+
+    cout << "X Vector (Solution):" << endl;
+    for (const auto &value : x)
     {
-
-        double diag = matrix[i][i];
-        if (abs(diag) > epsilon)
-        {
-            for (int j = 0; j < size * 2; j++)
-            {
-                matrix[i][j] /= diag;
-            }
-        }
+        cout << value << " ";
     }
-}
+    cout << endl;
 
-void put_identity(vector<vector<double>> &matrix)
-{
-    for (int i = 0; i < matrix.size(); i++)
-    {
-        for (int j = matrix.size(); j < matrix.size() * 2; j++, i++)
-        {
-            matrix[i][j] = 1;
-        }
-        cout << endl;
-    }
-}
-
-void inverse_matrix(vector<vector<double>> &matrix)
-{
-    put_identity(matrix);
-    gauss_eli(matrix);
-    jordan_eli(matrix);
-    row_eche(matrix);
+    return 0;
 }
 
 /*
 
-3
-1 1 -1
-1 -1 2
-2 1 1
+2 3 4 1 30
+1 1 2 3 20
+4 0 1 2 25
+3 2 0 1 15
 
 */
-
-int main()
-
-{
-    int no_of_variable = 0;
-    cin >> no_of_variable;
-
-    vector<vector<double>> matrix(no_of_variable, vector<double>(no_of_variable + no_of_variable, 0));
-
-    for (int i = 0; i < no_of_variable; i++)
-    {
-        for (int j = 0; j < no_of_variable; j++)
-        {
-            cin >> matrix[i][j];
-        }
-    }
-
-    inverse_matrix(matrix);
-
-    printing_inverse_matrix(matrix);
-
-    return 0;
-}
